ea53863d486644e41d7639fbdeaff7b2098deadf
ChunEunyu <xhoneyx01@naver.com>
2024-02-26 10:12:06 +0900

    fix: 태그 검색 페이지에서 태그를 누르면 각 태그에 맞는 애니메이션이 보여집니다

--- src/pages/Finder/components/ContentAria.jsx
+++ src/pages/Finder/components/ContentAria.jsx
@@ -18,7 +18,80 @@ const ContentAria = () => {
   
   // 선택된 태그와 일치하는 작품만 필터링합니다.
   const filterAnimeData = () => {
-    
+    const filteredData = []; // 필터링된 작품을 저장할 배열
+
+    animeData.forEach((data) => {
+      let shouldAdd = true; // 모든 태그를 만족하는지 여부
+      
+      checkedItem.forEach((tags, index) => {
+        if (tags.length !== 0) {
+          if (index === 0 || index === 1) { // 0: 장르, 1: 태그
+            const anime_tag_name = data.main_tag.map(tag => tag.name);
+            if (!tags.some(tag => anime_tag_name.includes(tag))) {
+              shouldAdd = false;
+              return;
+            } 
+
+          } else if (index === 2) { // 2: 년도
+            const year = parseInt(data.animation_info.air_year_quarter.slice(0, 4)); // 년도
+            const quarter = parseInt(data.animation_info.air_year_quarter.slice(5, 8)); // 분기
+            let tagMatched = false; // 태그가 일치하는지 여부를 나타내는 변수
+
+            tags.some((tag) => {
+              const tag_year = parseInt(tag.slice(0, 4)); // 년도
+              const tag_quarter = parseInt(tag.slice(5, 8)); // 분기
+
+              if (tag_year <= 2021 && tag_year >= 2010) { // 2010년 ~ 2021년
+                if (year === tag_year) {
+                  tagMatched = true;
+                  return true;
+                }
+              } else if (tag === '2000년대') { // 2000년대
+                if (year >= 2000 && year < 2010) {
+                  tagMatched = true;
+                  return true;
+                }
+              } else if (tag_year < 2000) {
+                if (year < 2000) {
+                  tagMatched = true;
+                  return true;
+                }
+              } else { // 2022년 1분기 ~ 2024년 1분기
+                if (tag_year === year && tag_quarter === quarter) {
+                  tagMatched = true;
+                  return true;
+                }
+              }
+            });
+
+            if (!tagMatched) {
+              shouldAdd = false;
+              return;
+            }
+            
+          } else if (index === 3) { // 3: 방영
+            if (((tags.some(tag => tag === '방영중')) && (data.is_ending === true)) || ((tags.some(tag => tag === '완결')) && (data.is_ending === false))) {
+              shouldAdd = false;
+              return;
+            }
+            
+          } else if (index === 4) { // 4: 출시 타입
+            if (tags.some(tag => tag !== data.animation_info.medium)) {
+              shouldAdd = false;
+              return; 
+            }
+          }
+        }
+      });
+
+      // 모든 태그 조건을 만족하는 경우만 배열에 추가
+      if (shouldAdd) {
+        filteredData.push(data);
+      }
+    });
+
+    // 필터링된 작품 목록을 저장
+    setFilteredAnimeData(filteredData);
   };
 
   useEffect(() => {
@@ -26,16 +99,17 @@ const ContentAria = () => {
   }, []);
 
   useEffect(() => {
-    filterAnimeData();
+    filterAnimeData(); 
+    console.log(checkedItem)
   }, [checkedItem]);
 
   return (
     <div className='w-full'>
       <span className='text-sm font-semibold mb-3'>
-        총 {animeData.length}개의 작품이 검색되었어요!
+        총 {filteredAnimeData.length}개의 작품이 검색되었어요!
       </span>
       <Row fluid lg={5} md={4} sm={3} xs={3}>
-        {animeData.map((anime)=>(
+        {filteredAnimeData.map((anime)=>(
           <Col>
             <AnimeCard 
               key={anime.id}
