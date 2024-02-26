e464cfbf1e03b816aa58a708a3259398994af3ca
ChunEunyu <xhoneyx01@naver.com>
2024-02-26 16:26:06 +0900

    fix: Loading 페이지에서 header를 제거

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import Spinner from 'react-bootstrap/Spinner';
-import Header from '../Header/PageHeader/Header';
 
 const Loading = () => {
   return (
-    <div>
-        <Header />
-        <div className="pt-40 flex justify-center items-start h-screen">
-            <Spinner animation="grow" />
-        </div>
+    <div className="pt-40 flex justify-center items-start h-screen">
+      <Spinner animation="grow" />
     </div>
   );
 }
