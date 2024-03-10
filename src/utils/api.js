0a9a1c497cb0e6e2de80c99e41edfaf163f1795e
ChunEunyu <xhoneyx01@naver.com>
2024-03-10 14:30:32 +0900

    Feat: cors 에러 해결

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ import { doc, setDoc, getDocs, collection } from "firebase/firestore";
 const header = {
   'Content-Type' : 'application/json',
   "Accept": "application/json",
-  "Access-Control-Allow-Origin": `http://localhost:3000`,
+  "Access-Control-Allow-Origin": `https://laftel-clone-project.vercel.app/`,
   'User-Agent' : 'Chrome/122.0.0.0 Mobile Safari/537.36',
   'laftel' : 'Tejava',
   'Access-Control-Allow-Credentials':"true",
